b40e195b7b4a9b93c07935930e302b01b295d200
sudeshnavg <71323212+sudeshnavg@users.noreply.github.com>
2022-04-24 11:30:31 +0530

    Static memory allocation
    Static memory allocation: Size of the memory is known during compilation time.

---
+++ sma.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+  int arr[50] = {10, 20, 30, 40, 50};
+  length = sizeof(arr)/sizeof(arr[0]);
+  int i;
+  for(i = 0; i < length; ++i)
+  {
+    printf("%d ", arr[i];
+  }
+  return 0;
+}
+
+  
